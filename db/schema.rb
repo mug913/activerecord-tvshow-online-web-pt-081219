92257b2adb000adc07320ff0e066812101a767c0
Brad Alexander <brad.alexander913@gmail.com>
2019-11-09 23:36:38 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "network"
+    t.string "day"
+    t.integer "rating"
+  end
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
